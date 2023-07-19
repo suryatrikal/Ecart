791a7252c87751aecd40daf3361c860b291ce403
Suryakant <tiwarisuryakant62@gmail.com>
2023-07-19 17:46:31 +0530

    second Commit

---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,19 @@
+// Create Token and saving in cookie
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJWTToken();
+  // console.log(token);
+  // options for cookie
+  const options = {
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+module.exports = sendToken;
